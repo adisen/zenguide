489723c7ad459cdf4bef9a78c7db6b8b16763a90
Israel <israeleadetunji@gmail.com>
2023-03-14 23:11:02 +0100

    add onboarding page 9

--- lib/onboarding/onboarding.dart
+++ lib/onboarding/onboarding.dart
@@ -612,6 +612,87 @@ class Onboarding8 extends StatelessWidget {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
+                            builder: (context) => const Onboarding9()),
+                      );
+                    })),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Onboarding9 extends StatelessWidget {
+  const Onboarding9({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF287bff),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(35.0, 0, 35.0, 60),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Column(
+              children: [
+                Text('Do you have a financial goal?',
+                    style: GoogleFonts.montserrat(
+                      textStyle: const TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w700,
+                          height: 1.4,
+                          fontFamily: 'Montserrat'),
+                    )),
+                Container(
+                  margin: const EdgeInsets.only(top: 20.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(right: 30.0),
+                        width: 100,
+                        height: 50,
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            side:
+                                const BorderSide(color: Colors.black, width: 1),
+                          ),
+                          child: const Text('Yes'),
+                        ),
+                      ),
+                      Container(
+                        width: 100,
+                        height: 50,
+                        child: TextButton(
+                          onPressed: () {},
+                          style: TextButton.styleFrom(
+                            foregroundColor: Colors.black,
+                            side: const BorderSide(
+                              color: Colors.black,
+                              width: 1,
+                            ),
+                          ),
+                          child: const Text('No'),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Container(
+                margin: const EdgeInsets.only(top: 100.0),
+                child: IconButton(
+                    icon: const Icon(Icons.arrow_right_alt_sharp),
+                    iconSize: 100,
+                    color: Colors.black,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
                             builder: (context) => const Onboarding6()),
                       );
                     })),
